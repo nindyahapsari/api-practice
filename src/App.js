8b45b7c4ee6f0d104ff0f15dd5278697482d2ae0
nindyahapsari <nindyahapsariiljas@yahoo.com>
2022-01-05 23:25:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const [string, setString] = useState("Initial state")
+  return (
+    <div className="App">
+     <h1>Api Practice</h1>
+
+    </div>
+  );
+}
+
+export default App;
